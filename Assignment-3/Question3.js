dc89cc1f09799113f49486a185a3556e2fcdbaff
Yash Bhardwaj <yash.bhardwaj@yashss481.successivetech.com>
2025-06-05 12:30:42 +0530

    fix: code updated

--- Assignment-3/Question3.js
+++ Assignment-3/Question3.js
@@ -1,13 +1,30 @@
 // 3. Write a program to display Diamond Pattern
 
-// *
-// *  *
-// *  *  *
-// *  *  *  *
-// *  *  *  *  *
-// *  *  *  *
-// *  *  *
-// *  *
-//  *
+function diamondpattern(){
+    let space = 4;
+    let star = 1;
+    
+    for(let i=1;i <= 9 ;i++){
+        let row = "";
+        for(let j= space; j >= 0 ;j--){
+            row += " ";
+        }
+        for(let j=1;j <= star ;j++){
+            row += "*";
+            row += " ";
+        }
+
+        if(i<5){
+            space--;
+            star++;
+        }else{
+            space++;
+            star--;
+        }
+        console.log(row);
+    }
+}
+
+diamondpattern();
 
 
