e11e0a5d616ec587de11ae7b03dd615e0385805f
Yash Bhardwaj <yash.bhardwaj@yashss481.successivetech.com>
2025-06-10 18:41:11 +0530

    feat:Assignment Completion

--- Assignment-6/Question3.js
+++ Assignment-6/Question3.js
@@ -2,8 +2,6 @@
 // 3.2 Create a class Student that inherits from the Person class and has a property studies. Override the greeting() method to include information about what the student is studying.
 // 3.3 Create a class Teacher that inherits from the Person class and has a property subjectsTaught. Override the farewell() method to include information about what the teacher teaches.
 
-
-
 class Person {
   constructor(name, age, gender, interests) {
     this.name = name;
@@ -13,33 +11,35 @@ class Person {
   }
 
   greeting() {
-    console.log(
-      `This Person name is :${this.name} and age: ${this.age} and is ${this.gender} with Interest in ${this.interests}`
-    );
+    return `This Person name is :${this.name} and age: ${this.age} and is ${this.gender} with Interest in ${this.interests}`;
   }
 
   farewell() {
-    console.log(`goodbye ${this.name}`);
+    return `goodbye ${this.name}`;
   }
 }
 
-class Student extends Person{
-    constructor(studies){
-        this.studies= studies;
-    }
+class Student extends Person {
+  constructor(name, age, gender, interests, studies) {
+    super(name, age, gender, interests);
+    this.studies = studies;
+  }
 
-    greeting(){
-        console.log(`Student is Studying ${this.studies}`);
-    }
+  greeting() {
+    return `${super.greeting()} and studies ${this.studies}`;
+  }
 }
 
-class Teacher extends Person{
-    constructor(subjectTaught){
-        this.subjectTaught = subjectTaught;
-    }
+class Teacher extends Person {
+  constructor(name, age, gender, interests, subjectTaught) {
+    super(name, age, gender, interests);
+    this.subjectTaught = subjectTaught;
+  }
 
-    farewell(){
-        console.log(`Teacher Teaches ${this.subjectTaught}`);
-    }
+  farewell() {
+    return `${super.farewell()} and Teaches ${this.subjectTaught}`;
+  }
 }
 
+let st = new Student("yash", 22, "male", "chess", "maths");
+console.log(st.greeting());
