44a9aedf9a2fb92cbf6dc5dbb51526e191e4c646
Yash Bhardwaj <yash.bhardwaj@yashss481.successivetech.com>
2025-06-10 15:32:27 +0530

    fix:code optimize

--- Assignment-5/Question4.js
+++ Assignment-5/Question4.js
@@ -1,12 +1,33 @@
 // 4. Write a program deepClone that takes an object as input and returns a deep copy of that object. The function should handle nested objects and arrays.
 
-const obj1 = {
-  name: "Yash",
-  company: "successive",
-  age: 22,
-};
+const obj= {
+    name:"Yash",
+    age:21,
+    country: {
+        asia: "India",
+        africa: "south africa"
+    }
+}
 
-const shallowcopy = { ...obj1 };
-shallowcopy.name = "yash";
-console.log(obj1);
-console.log(shallowcopy);
+// const shallowcopy = {...obj};
+// shallowcopy.country.asia = "pakistan";
+
+// console.log(obj);
+// console.log(shallowcopy);
+
+
+// const deepcopy = JSON.parse(JSON.stringify(obj));
+// deepcopy.country.asia = "afganistan";
+
+// console.log(obj);
+// console.log(deepcopy);
+
+function deepclone(obj){
+    const deepcopy = JSON.parse(JSON.stringify(obj));
+    return deepcopy;
+}
+
+deepcopy.country.asia = "china";
+
+console.log(obj);
+console.log(deepclone(obj));
