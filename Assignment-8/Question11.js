0ddb46b8398950f9495a917f2743d2299801ed7b
Yash Bhardwaj <yash.bhardwaj@yashss481.successivetech.com>
2025-06-13 16:59:41 +0530

    fix: Assignment Completion done

--- Assignment-8/Question11.js
+++ Assignment-8/Question11.js
@@ -1,10 +1,23 @@
- var promise = new Promise(function(resolve, reject) {
-    setTimeout(function() { resolve("Data received"); }, 1000);
- });
- promise.then(function(data) {
+// var promise = new Promise(function(resolve, reject) {
+//    setTimeout(function() { resolve("Data received"); }, 1000);
+// });
+// promise.then(function(data) {
+//    console.log(data);
+// }, function(error) {
+//    console.log(error);
+// });
+
+// Rectification
+
+var promise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve("Data received");
+  }, 1000);
+});
+promise
+  .then(function (data) {
     console.log(data);
- }, function(error) {
+  })
+  .catch((error) => {
     console.log(error);
- });
- 
- 
+  });
